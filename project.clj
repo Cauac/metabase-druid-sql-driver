52cac680f02b81f1b4cd1d0b241e0314cd69d026
Anton Garansky <anton.garansky@telenordigital.com>
2023-04-20 22:27:18 +0200

    remove clojure.jdbc/clojure.jdbc-c3p0 dependency

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
   :repl-options {:init-ns metabase.driver.druid-sql}
 
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]
-                                       [metabase/metabase "0.45.3"]
-                                       [clojure.jdbc/clojure.jdbc-c3p0 "0.3.3"]]}
+                                       [metabase/metabase "0.45.3"]]}
              :uberjar  {:auto-clean   true
                         :aot          :all
                         :javac-options ["-target" "1.8", "-source" "1.8"]
